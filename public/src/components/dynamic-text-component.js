8a34b33cd1ecdccb1620de40ccc329d91dae88e2
Zack <zack@ukaton.com>
2023-08-26 15:46:47 -0700

    dynamic-text

---
+++ public/src/components/dynamic-text-component.js
@@ -0,0 +1,127 @@
+AFRAME.registerComponent("dynamic-text", {
+  schema: {
+    text: { type: "string", default: "" },
+    wordWrap: { type: "number", default: 20 },
+    width: { type: "number", default: 0.5 },
+    opacity: { type: "number", default: 1 },
+    backgroundOpacity: { type: "number", default: 1 },
+    color: { type: "color", default: "black" },
+    backgroundColor: { type: "color", default: "white" },
+    align: {
+      type: "string",
+      default: "center",
+      oneOf: ["left", "center", "right"],
+    },
+  },
+
+  init: function () {
+    this.textSize = new THREE.Vector3();
+
+    this.textEntity = document.createElement("a-text");
+    this.textEntity.setAttribute("word-wrap", this.data.wordWrap);
+    this.textEntity.setAttribute("color", this.data.color);
+    this.textEntity.setAttribute("shader", "flat");
+    this.textEntity.setAttribute("align", this.data.align);
+    this.textEntity.setAttribute("width", this.data.width);
+    this.textEntity.setAttribute("value", this.data.text);
+    this.el.appendChild(this.textEntity);
+
+    this.planeEntity = document.createElement("a-plane");
+    this.planeEntity.setAttribute(
+      "geometry",
+      "primitive: plane; width: 0; height: 0;"
+    );
+    this.planeEntity.setAttribute(
+      "material",
+      `color: ${this.data.backgroundColor}; shader: flat; opacity: 1`
+    );
+    this.el.appendChild(this.planeEntity);
+  },
+  update: function (oldData) {
+    const diff = AFRAME.utils.diff(oldData, this.data);
+
+    const diffKeys = Object.keys(diff);
+
+    diffKeys.forEach((key) => {
+      switch (key) {
+        case "text":
+          this.updateText();
+          break;
+        case "align":
+          break;
+        case "color":
+          this.updateColor();
+          break;
+        case "backgroundColor":
+          this.updateBackgroundColor();
+          break;
+        case "opacity":
+          break;
+        case "backgroundOpacity":
+          this.updateBackgroundOpacity();
+          break;
+        case "width":
+        case "wordWrap":
+          break;
+        default:
+          console.log("uncaught key", key);
+      }
+    });
+  },
+
+  updateText: function () {
+    // FILL - update text and plane
+    this.updateTextSize();
+  },
+  updateTextSize: function () {
+    if (!this.waitedToLoad) {
+      setTimeout(() => {
+        this.waitedToLoad = true;
+        this.updateTextSize();
+      }, 500);
+      return;
+    }
+
+    if (this.el.hasLoaded) {
+      setTimeout(() => {
+        const { textSize, planeEntity, textEntity } = this;
+
+        let text = textEntity.components["text"];
+        text.geometry.computeBoundingBox();
+        const { boundingBox } = text.geometry;
+        boundingBox.getSize(textSize);
+
+        const width = textSize.x / 1000;
+        const height = textSize.y / 1000;
+        planeEntity.setAttribute("width", width);
+        planeEntity.setAttribute("height", height);
+        if (text.data.baseline !== "center") {
+          planeEntity.object3D.position.y =
+            ((text.data.baseline == "top" ? -1 : 1) * height) / 2;
+        }
+        console.log(width, height);
+      }, 0);
+    } else {
+      this.el.addEventListener("loaded", () => {
+        this.updateTextSize();
+      });
+    }
+  },
+  updateColor: function () {
+    this.textEntity.setAttribute("color", this.data.color);
+  },
+  updateBackgroundColor: function () {
+    this.planeEntity.setAttribute(
+      "material",
+      "color",
+      this.data.backgroundColor
+    );
+  },
+  updateBackgroundOpacity: function () {
+    this.planeEntity.setAttribute(
+      "material",
+      "opacity",
+      this.data.backgroundOpacity
+    );
+  },
+});
